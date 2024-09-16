e1ae4b2b859e7871284110e6b48bd6db8465e15f
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-16 09:38:39 -0400

    Add project files.

---
+++ Topic 4 - Lesson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Topic_4___Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String userName;
+            int age;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter username");
+            userName = Console.ReadLine();
+            Console.Write("Your username is " + userName);
+            Console.WriteLine("Andre");
+            Console.ReadLine();
+            Console.WriteLine("Enter your age.");
+            Console.WriteLine (Int32.TryParse(Console.ReadLine(), out age));
+            Console.WriteLine("Your age is " + age);
+            Console.ReadLine();
+        }
+        
+    }
+}
