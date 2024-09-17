5dec637b95426a8d5179c513e22c3b64c779dd16
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-17 09:38:38 -0400

    Not done

--- Topic 4 - Lesson/Program.cs
+++ Topic 4 - Lesson/Program.cs
@@ -6,18 +6,23 @@ namespace Topic_4___Lesson
     {
         static void Main(string[] args)
         {
-            String userName;
+            //Andre
+            string userName;
             int age;
+            double price;
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Enter username");
             userName = Console.ReadLine();
-            Console.Write("Your username is " + userName);
-            Console.WriteLine("Andre");
+            Console.Write("Your username is: " + userName);
+            Console.WriteLine("");
             Console.ReadLine();
             Console.WriteLine("Enter your age.");
             Console.WriteLine (Int32.TryParse(Console.ReadLine(), out age));
             Console.WriteLine("Your age is " + age);
             Console.ReadLine();
+            Console.WriteLine("What is the price");
+            Double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine("The price is " + (price * 0.8).ToString("C"));
         }
         
     }
