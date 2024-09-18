b1aa2fb770a1355787ddd304a9c66912a9c4e2d2
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-18 09:38:31 -0400

    not done yet

--- Topic 4 - Lesson/Program.cs
+++ Topic 4 - Lesson/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Win32.SafeHandles;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Security;
 
 namespace Topic_4___Lesson
 {
@@ -10,6 +13,10 @@ namespace Topic_4___Lesson
             string userName;
             int age;
             double price;
+            string pizzatop;
+            string name;
+            pizzatop = "BRAK";
+            name = "Jimmy";
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Enter username");
             userName = Console.ReadLine();
@@ -23,6 +30,14 @@ namespace Topic_4___Lesson
             Console.WriteLine("What is the price");
             Double.TryParse(Console.ReadLine(), out price);
             Console.WriteLine("The price is " + (price * 0.8).ToString("C"));
+            Console.ReadLine();
+            Console.WriteLine("What's up man, whats your name? ");
+            name = Console.ReadLine();
+            Console.WriteLine("OHHHHHHH NO WAYYYYYY IT'S REALY YOU " + name );
+            Thread.Sleep(2000);
+            Console.WriteLine("Wait whats your fav topng on pizza again?" );
+            pizzatop = Console.ReadLine();
+            Console.WriteLine("NO WAY " + pizzatop + " IS MY FAV TOO"); 
         }
         
     }
